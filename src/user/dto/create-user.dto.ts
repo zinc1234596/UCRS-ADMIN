75a5df9af6f481cba81b8ad4f0a8124cbe49bf8d
ryan.mac <1695073263@qq.com>
2022-12-20 17:00:55 +0800

    swagger doc

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({ example: 'user1' })
   username: string;
+  @ApiProperty({ example: 'abc123456' })
   password: string;
 }
