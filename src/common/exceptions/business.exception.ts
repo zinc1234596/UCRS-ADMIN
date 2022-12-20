bb6c2869acb4d995460a32b207fcd299dd950834
ryan.mac <1695073263@qq.com>
2022-12-20 14:32:58 +0800

    全局异常拦截/异常过滤器

---
+++ src/common/exceptions/business.exception.ts
@@ -0,0 +1,26 @@
+// 处理业务运行中预知且主动抛出的异常
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { BUSINESS_ERROR_CODE } from '@/common/constants/business.error.codes.constants';
+
+type BusinessError = {
+  code: number;
+  message: string;
+};
+export class BusinessException extends HttpException {
+  constructor(err: BusinessError | string) {
+    if (typeof err === 'string') {
+      err = {
+        code: BUSINESS_ERROR_CODE.COMMON,
+        message: err,
+      };
+    }
+    super(err, HttpStatus.OK);
+  }
+
+  static throwForbidden() {
+    throw new BusinessException({
+      code: BUSINESS_ERROR_CODE.ACCESS_FORBIDDEN,
+      message: '无此权限！',
+    });
+  }
+}
