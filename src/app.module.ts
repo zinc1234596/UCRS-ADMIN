953da5e8ed1ec9a8cd2136cf7f0b3b16927335e9
ryan.mac <1695073263@qq.com>
2022-12-20 22:01:18 +0800

    swagger doc

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,27 @@ import { AppService } from './app.service';
 import { getConfig } from './utils';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from '@/common/guards/auth.guard';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      // 禁用默认读取 .env 的规则
       ignoreEnvFile: true,
       isGlobal: true,
       load: [getConfig],
     }),
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
