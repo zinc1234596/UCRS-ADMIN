6a07c37db36fae7e35d09ed28969577fd9d597b5
ryan.mac <1695073263@qq.com>
2022-12-26 17:47:07 +0800

    swagger调整

--- src/user/dto/login-user.dto.ts
+++ src/user/dto/login-user.dto.ts
@@ -2,12 +2,12 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
 
 export class LoginUserDto {
-  @ApiProperty({ description: '账号' })
+  @ApiProperty({ description: '账号', example: 'Mary' })
   @IsString({ message: 'username 类型错误' })
   @IsNotEmpty({ message: '账号不能为空' })
   username: string;
 
-  @ApiProperty({ description: '密码' })
+  @ApiProperty({ description: '密码', example: '64' })
   @IsString({ message: 'password 类型错误' })
   @IsNotEmpty({ message: '密码不能为空' })
   password: string;
